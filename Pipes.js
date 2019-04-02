50117b1a24ca3c628fa2fc44ab2b760e5b081341
JDAIN <poker98@web.de>
2019-04-02 15:37:11 +0200

    performance improvements and added pipe sprites

--- Pipes.js
+++ Pipes.js
@@ -1,43 +1,43 @@
 class Pipes {
     constructor() {
+        //TODO implement deque with len 5 which wraps for better performance
         this.pipes = [];
         this.xspeed = 2.5;
         this.x = width;
         this.gap = 130;
+        //divide height by 4 and select first and last
+        this.gapStart = height / 4;
+        this.gapEnd = this.gapStart * 3 - this.gap;
     }
 
     draw() {
         for (let i = 0; i < this.pipes.length; i++) {
-            rect(...this.pipes[i]["top"]);
-            rect(...this.pipes[i]["bottom"]);
+            this.pipes[i]["top"][0] -= this.xspeed;
+            this.pipes[i]["bottom"][0] -= this.xspeed;
+            //top
+
+            image(topPipImg, ...this.pipes[i]["top"]);
+            //bottom
+            image(botPipeImg, ...this.pipes[i]["bottom"]);
         }
+
+
     }
 
     update() {
         //every 55 frames new pipe
-        if (frameCount % 55 === 0) {
-            let quarterWidth= height/4;
-            let gapStart= quarterWidth;
-            let gapEnd= quarterWidth*3-this.gap;
-            let randomStartGapY= Math.random() * (gapEnd - gapStart + 1)  + gapStart;
-
+        if (frameCount % 55 == 0) {
+            let randomStartGapY = Math.random() * (this.gapEnd - this.gapStart) + this.gapStart;
             this.pipes.push(
                 {
                     "top":
-                        [this.x, -1, 40, randomStartGapY],
+                        [this.x, randomStartGapY, 52, -topPipImg.height],
                     "bottom":
-                        [this.x, randomStartGapY+this.gap, 40, height]
+                        [this.x, randomStartGapY + this.gap, 52, botPipeImg.height]
                 });
         }
         if (this.pipes.length > 4) {
             this.pipes.shift();
         }
-        for (let i = 0; i < this.pipes.length; i++) {
-            this.pipes[i]["top"][0] -= this.xspeed;
-            this.pipes[i]["bottom"][0] -= this.xspeed;
-
-        }
     }
-
-
 }
--- sketch.js
+++ sketch.js
@@ -1,33 +1,41 @@
+
 let bird;
 let pipes;
 let backgroundImg;
-let baseImg;
+
+let botPipeImg;
+let topPipImg;
+
 function preload(){
     backgroundImg = loadImage('images/background-day.png');
     base = loadImage('images/base.png');
-
+    botPipeImg = loadImage('images/pipe-green.png');
+    topPipImg =loadImage('images/pipe-green-top.png');
 }
 
 function setup() {
   // put setup code here
     createCanvas(450,600);
-    console.log(backgroundImg);
     background(backgroundImg);
     drawBase();
     bird = new Bird();
     pipes = new Pipes();
+
 }
 
 function draw() {
     background(backgroundImg);
-
   // put drawing code here
     bird.draw();
     bird.update();
     pipes.update();
     pipes.draw();
+    drawBase(); //draws base
 
-    drawBase() //draws base
+    let fps = frameRate();
+    fill(0);
+    stroke(0);
+    text("FPS: " + fps.toFixed(2), 10, height - 10);
 
 
 }
