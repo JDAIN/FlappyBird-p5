d011c0f4917e81518c46f848234dc7dadc5b0c9d
JDAIN <poker98@web.de>
2019-04-02 14:36:20 +0200

    added background img and base img

--- sketch.js
+++ sketch.js
@@ -1,25 +1,39 @@
 let bird;
 let pipes;
+let backgroundImg;
+let baseImg;
+function preload(){
+    backgroundImg = loadImage('images/background-day.png');
+    base = loadImage('images/base.png');
 
+}
 
 function setup() {
   // put setup code here
-    createCanvas(450,650);
-    background("deepskyblue");
+    createCanvas(450,600);
+    console.log(backgroundImg);
+    background(backgroundImg);
+    drawBase();
     bird = new Bird();
     pipes = new Pipes();
 }
 
 function draw() {
-    background("deepskyblue");
+    background(backgroundImg);
+
   // put drawing code here
     bird.draw();
     bird.update();
     pipes.update();
     pipes.draw();
 
+    drawBase() //draws base
 
 
+}
+
+function drawBase(){
+   return image(base,0,height-base.height+30, width, base.height);
 }
 function keyPressed(){
     if(keyCode = "UP_ARROW"){
@@ -27,6 +41,6 @@ function keyPressed(){
     }
 }
 function mousePressed() {
-    console.log();
+
     bird.up();
 }
